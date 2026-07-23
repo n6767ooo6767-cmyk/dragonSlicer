b54c57be17b549b7392475f18a9d70d97d1acd58
n6767ooo6767-cmyk <n6767ooo6767@gmail.com>
2026-07-23 13:23:59 +0200

    Create script.js

---
+++ app/script.js
@@ -0,0 +1,36 @@
+// 🚀 NovaSlicer v0.0.1
+
+const fileInput = document.getElementById("modelFile");
+const sliceButton = document.querySelector("button");
+
+
+// Загрузка модели
+fileInput.addEventListener("change", () => {
+    const file = fileInput.files[0];
+
+    if (file) {
+        alert(
+            "🧊 Модель загружена:\n" + file.name
+        );
+    }
+});
+
+
+// Кнопка Slice
+sliceButton.addEventListener("click", () => {
+
+    const file = fileInput.files[0];
+
+    if (!file) {
+        alert("⚠️ Сначала выбери STL/OBJ/3MF модель!");
+        return;
+    }
+
+
+    alert(
+        "🔪 Начинаем нарезку...\n\n" +
+        "Модель: " + file.name +
+        "\n\nNovaSlicer v0.0.1"
+    );
+
+});
